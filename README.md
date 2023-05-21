253a835aa0f7cca40b7659112779e90ae1a75b42
k3llydev <hello@k3lly.dev>
2023-05-21 10:22:39 -0600

    Initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Better Friendly No Kill
-
-An Overwatch 2 workshop configuration. This is WIP.
--- utils/helpers.ts
+++
@@ -1,3 +0,0 @@
-export const helpers = {
-    renderIf: (condition: boolean, content: string) => condition ? content : ''
-};
