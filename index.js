d06968cf86accb5e3956755ad1adbb564645b65d
k3llydev <hello@k3lly.dev>
2023-05-21 11:05:32 -0600

    fix(js): response validation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const releases = await fetch('https://api.github.com/repos/k3llydev/better-friendly-no-kill/releases');
+    const releases = await fetch('https://api.github.com/repos/k3llydev/better-friendly-no-kill/releases')
+        .then(response => response.json())
+        .catch(error => console.error(error));
     console.log('releases', releases);
 });
